0d04ebb9cf8ffbff88da50c86a228984932f3480
Anturia Hassani <anturia.hm@gmail.com>
2024-04-24 12:20:44 +0200

    Ajout de fonctionnalité d'ajout d'une nouvelle tâche

--- js/todolist.js
+++ js/todolist.js
@@ -1,6 +1,8 @@
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 const li = document.getElementsByClassName("task");
 const suppr = document.getElementsByClassName("suppr");
+const form = document.querySelector("form");
+const list = document.getElementById("list");
 
 checkboxes.forEach(function (checkbox) {
     checkbox.addEventListener('change', handleCheckboxChange);
@@ -17,14 +19,27 @@ function handleCheckboxChange(event) {
     }
 }
 
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let newtask = document.getElementById("tasktoadd").value;
+    let toadd = document.createElement('li');
+    toadd.className = "task";
+    toadd.innerHTML = '<input type="checkbox"><label for="task">' + newtask + '</label><input type="button" value="suppr" class="suppr">';
+    list.appendChild(toadd);
+    
+}
+)
+
+
 for (let i = 0; i < suppr.length; i++) {
     suppr[i].addEventListener('click', function (event) {
         const btnSuppr = event.target;
-
         btnSuppr.parentNode.remove();
     }
     );
 }
+
+
 
 
 
