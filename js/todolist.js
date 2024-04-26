3cf43f52baebf378b33235aeffca471ad7e9315f
Anturia Hassani <anturia.hm@gmail.com>
2024-04-26 10:30:55 +0200

    Fonctionnalité de tâche barrée lorsque user coche une tâche

--- js/todolist.js
+++ js/todolist.js
@@ -1,43 +1,54 @@
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 const li = document.getElementsByClassName("task");
 const suppr = document.getElementsByClassName("suppr");
+const supprtout = document.getElementById("supprtout");
 const form = document.querySelector("form");
 const list = document.getElementById("list");
 
-checkboxes.forEach(function (checkbox) {
-    checkbox.addEventListener('change', handleCheckboxChange);
-});
-
-function handleCheckboxChange(event) {
-    const checkbox = event.target;
-    const label = checkbox.nextElementSibling;
-
-    if (checkbox.checked) {
-        label.style.textDecoration = "line-through";
-    } else {
-        label.style.textDecoration = "none";
-    }
-}
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
     let newtask = document.getElementById("tasktoadd").value;
     let toadd = document.createElement('li');
     toadd.className = "task";
-    toadd.innerHTML = '<input type="checkbox"><label for="task">' + newtask + '</label><input type="button" value="suppr" class="suppr">';
+    toadd.innerHTML = '<input type="checkbox" name="task"><label for="task">' + newtask + '</label><input type="button" value="suppr" class="suppr">';
     list.appendChild(toadd);
-    
+    const tasks = document.querySelectorAll('input[name="task"]');
+    tasks.forEach(function (task) {
+        task.addEventListener('change', handleCheckboxChange);
+    });
+
+    for (let i = 0; i < suppr.length; i++) {
+        suppr[i].addEventListener('click', deleteTask); 
+    }
 }
 )
 
-
+supprtout.addEventListener('click', function () {
 for (let i = 0; i < suppr.length; i++) {
-    suppr[i].addEventListener('click', function (event) {
-        const btnSuppr = event.target;
-        btnSuppr.parentNode.remove();
-    }
-    );
+    suppr[i].parentNode.remove();
 }
+});
+
+
+
+function handleCheckboxChange(event) {
+    const task = event.target;
+    const label = task.nextElementSibling;
+
+    if (task.checked) {
+        label.style.textDecoration = "line-through";
+    } else {
+        label.style.textDecoration = "none";
+    }
+}
+
+
+function deleteTask (event) {
+    const btnSuppr = event.target;
+    btnSuppr.parentNode.remove();
+}
+
+
 
 
 
